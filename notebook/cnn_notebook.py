dcd35891ae8cb7094860f4a7f485857957729a70
Sage <sageginther@gmail.com>
2026-04-07 17:00:43 -0400

    pushing training print message

--- notebook/cnn_notebook.py
+++ notebook/cnn_notebook.py
@@ -22,42 +22,37 @@ def _():
     from src.models.CNN import CNN
     from src.data.read_data import get_dataloaders
     from src.data.preprocessors import DATA_TRANSFORM, DATA_TRANSFORM_DOWNSAMPLE
+    from src.engine.train_loop import train_model
 
-    return CNN, DATA_TRANSFORM, get_dataloaders, nn, optim, torch
+    return CNN, DATA_TRANSFORM, get_dataloaders, nn, optim, torch, train_model
 
 
 @app.cell
 def _(DATA_TRANSFORM, get_dataloaders):
     train_loader, val_loader, _ = get_dataloaders(DATA_TRANSFORM)
-    return (train_loader,)
+    return train_loader, val_loader
 
 
 @app.cell
 def _(CNN, nn, optim):
-    model = CNN(num_classes=10)
+    model = CNN(num_classes=10, dropout_rate=0.5)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4) #learning rate 
+    optimizer = optim.Adam(model.parameters(), lr=0.01) #learning rate 
     return criterion, model, optimizer
 
 
 @app.cell
-def _(criterion, model, optimizer, train_loader):
-    num_epochs = 5  #change as needed, paper mentioned up to 105 :O
+def _(criterion, model, optimizer, train_loader, train_model, val_loader):
+    num_epochs = 50  #change as needed, paper mentioned up to 100 :O
 
-    for epoch in range(num_epochs):
-        model.train()
-        running_loss = 0.0
-        for images, labels in train_loader:
-            images, labels = images, labels
-
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {running_loss/len(train_loader):.4f}") #prints out corss-entropy loss per epoch
+    losses = train_model(
+            model=model,
+            train_loader=train_loader,
+            val_loader=val_loader,
+            epochs=num_epochs,
+            loss_fn=criterion,
+            optimizer=optimizer
+        )
     return
 
 
